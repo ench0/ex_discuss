5318cfca07f469faa14c312e2b94e74e130e133b
Ensar <ensar@farend.net>
2016-11-19 15:15:49 +0000

    edit

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -3,6 +3,7 @@ defmodule Discuss.TopicController do
 
   alias Discuss.Topic
 
+#NEW
   def new(conn, _params) do
     struct = %Topic{} #because of alias, instead of: struct = %Discuss.Topic{}
     params = %{}
@@ -24,27 +25,56 @@ defmodule Discuss.TopicController do
 #    %{"topic" => topic} = params
 #  end
 
+#CREATE
   def create(conn, %{"topic" => topic}) do
     changeset = Topic.changeset(%Topic{}, topic)
 
     sum = 2 + 2
 
     case Repo.insert changeset do
-      {:ok, post} ->
-        IO.inspect(post)
+      {:ok, topic} ->
+        IO.inspect(topic)
         conn
-        |> put_flash(:info, "Topic Created")#???<> %Discuss.Topic.title
+        |> put_flash(:info, "Topic \"" <> topic.title <> "\" Created")
         |> redirect(to: topic_path(conn, :index))
         #render conn, "index.html", changeset: changeset, sum: sum, error: :error
       #{:error, changeset} -> IO.inspect(changeset)
       {:error, changeset} -> 
-        render conn, "new.html", changeset: changeset, sum: sum, error: :error
+        conn
+        |> put_flash(:alert, "Something went wrong")#alert because we already used :error atom, needs change in the layout
+        |> render "new.html", changeset: changeset, sum: sum, error: :error
     end
   end
 
+#LIST
   def index(conn, _params) do
     topics = Repo.all Topic#, order_by: Topic.title, limit: 3
     render conn, "index.html", topics: topics
+  end
+
+#EDIT
+  def edit(conn, %{"id" => topic_id}) do #params = %{"id" => topic_id}
+    topic = Repo.get(Topic, topic_id)
+    changeset = Topic.changeset(topic)
+
+    render conn, "edit.html", changeset: changeset, topic: topic    
+  end
+
+#UPDATE
+  def update(conn, %{"id" => topic_id, "topic" => topic}) do
+    old_topic = Repo.get(Topic, topic_id)
+    changeset = Topic.changeset(old_topic, topic)#first argument in changeset is struct, existing record in our database, second argument is new attribute
+    #changeset = Repo.get(Topic, topic_id) |> Topic.changeset(topic)#using pipe we simplify the above, but need old_topic var
+    
+    case Repo.update(changeset) do
+      {:ok, _topic} ->
+        conn
+        |> put_flash(:info, "Topic Updated")
+        |> redirect(to: topic_path(conn, :index))
+      {:error, changeset} ->
+        render conn, "edit.html", changeset: changeset, topic: old_topic
+    end
+
   end
 
 end
--- web/router.ex
+++ web/router.ex
@@ -20,6 +20,8 @@ defmodule Discuss.Router do
     get "/topics/new", TopicController, :new
     post "/topics", TopicController, :create
     get "/topics", TopicController, :index
+    get "/topics/:id/edit", TopicController, :edit
+    put "/topics/:id/edit", TopicController, :update
   end
 
   # Other scopes may use custom stacks.
